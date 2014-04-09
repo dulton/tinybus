aea460373db8e88329e71b74dcb8016ff5f891c8
Shiyong Zhang <shiyong.zhang.cn@gmail.com>
2014-04-09 20:54:53 +0800

    add bus operation

--- tinybus.c
+++ tinybus.c
@@ -3,5 +3,265 @@
  * Copyright by Zhang Shiyong, 2014. shiyong.zhang.cn@outlook.com
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "tinybus.h"
 
+#define tiny_bus_print_err(err, name) \
+do {\
+	int error = (err); 							\
+	if (error)	 		 		 			\
+		fprintf(stderr, "file %s: line %d (%s): error '%d' during '%s'",	\
+           __FILE__, __LINE__, __FUNCTION__, error, name);					\
+} while (0)
+
+static int compare_message_id(void *node, void *data)
+{
+	message_id_t *id;
+	tiny_msg_t *msg;
+
+	return (msg->msg_id == *id) ? 0 : 1;
+}
+
+static void
+post_message(void *node, void *data)
+{
+	slot_t *slot;
+	tiny_msg_t *msg;
+	
+	slot = (slot_t *)node;
+	msg = (tiny_msg_t *)data;
+	
+	// now check whether msg->msg_id is in slot->subscription
+	if (list_find_custom(slot->subscription, msg, compare_message_id) != NULL)
+	{
+		async_queue_push(slot->msg_queue, msg);		
+		atomic_inc(&msg->msg_priv->ref_count);
+	}
+}
+
+static int
+tiny_bus_deliver(tiny_bus_t *self, tiny_msg_t *msg)
+{
+	assert(self->mutex);
+	
+	pthread_mutex_lock(self->mutex);
+	list_foreach(self->slot_list, post_message, msg);
+	pthread_mutex_unlock(self->mutex);	
+}
+
+static void *
+tiny_bus_thread_worker(void *context)
+{
+	tiny_msg_t *msg;
+	tiny_bus_t *self;
+
+	self = (tiny_bus_t *)context;	
+	for (;;)
+	{
+		msg = (tiny_msg_t *)async_queue_pop(self->msg_queue);
+		if (msg->msg_id == TINY_BUS_MSG_EXIT)
+		{
+			fprintf(stdout, "file %s: line %d (%s): bus exit",
+				__FILE__, __LINE__, __FUNCTION__);				
+			break;
+		}
+		
+		tiny_bus_deliver(self, msg);
+		atomic_dec(&msg->msg_priv->ref_count);
+	}
+	
+	return NULL;
+}
+
+static void
+tiny_bus_free0(tiny_bus_t *bus)
+{
+	assert(bus);
+	
+	if (bus->msg_queue)
+		async_queue_destroy(bus->msg_queue);
+	
+	if (bus->mutex)
+		free(bus->mutex);
+		
+	if (bus->slot_list)
+		list_free(bus->slot_list);
+		
+	if (bus->thread)
+		free(bus->thread);
+		
+	free(bus);
+}
+
+tiny_bus_t*
+tiny_bus_new(void)
+{
+	tiny_bus_t *bus;
+	int result;
+	
+	bus = (tiny_bus_t *)calloc(1, sizeof(tiny_bus_t));
+	if (bus == NULL)
+	{
+		tiny_bus_print_err(errno, "calloc");			
+		return NULL;	
+	}
+	
+	bus->msg_queue = async_queue_new();
+	if (bus->msg_queue == NULL)
+	{
+		tiny_bus_print_err(errno, "async_queue_new");	
+		tiny_bus_free0(bus);
+		return NULL;
+	}
+		
+	bus->mutex = (pthread_mutex_t *)calloc(1, sizeof(pthread_mutex_t));
+	if (bus->mutex == NULL)
+	{
+		tiny_bus_print_err(errno, "calloc");
+		tiny_bus_free0(bus);
+		return NULL;
+	}
+	
+	result = pthread_mutex_init(bus->mutex, NULL);
+	if (result != 0)
+	{
+		tiny_bus_print_err(errno, "pthread_mutex_init");
+		tiny_bus_free0(bus);
+		return NULL;
+	}
+	
+	bus->slot_list = (list_t *)calloc(1, sizeof(list_t));
+	if (bus->slot_list == NULL)
+	{
+		tiny_bus_print_err(errno, "calloc");
+		tiny_bus_free0(bus);
+		return NULL;		
+	}
+	
+	bus->thread = (pthread_t *)calloc(1, sizeof(pthread_t));
+	if (bus->thread == NULL)
+	{
+		tiny_bus_print_err(errno, "calloc");
+		tiny_bus_free0(bus);
+		return NULL;		
+	}
+	
+	result = pthread_create(bus->thread, NULL, tiny_bus_thread_worker, bus);
+	if (result)
+	{
+		tiny_bus_print_err(result, "pthread_create");
+		tiny_bus_free0(bus);
+		return NULL;		
+	}
+	
+	return bus;
+}
+
+void
+tiny_bus_destroy(tiny_bus_t *bus)
+{
+	// .. to do
+}
+
+tiny_bus_result_t
+tiny_bus_add_slot(tiny_bus_t *bus, slot_t *slot)
+{
+	int result;
+	
+	assert(bus);
+	assert(bus->mutex);
+	
+	result = pthread_mutex_lock(bus->mutex);
+	if (result)
+		return TINY_BUS_FAILED;
+		
+	list_insert_tail(bus->slot_list, slot);
+	
+	result = pthread_mutex_unlock(bus->mutex);
+	if (result)
+		return TINY_BUS_FAILED;
+		
+	return TINY_BUS_SUCCEED;
+}
+
+tiny_bus_result_t
+tiny_bus_del_slot(tiny_bus_t *bus, slot_t *slot)
+{
+	int result;
+	
+	assert(bus);
+	assert(bus->mutex);
+	
+	result = pthread_mutex_lock(bus->mutex);
+	if (result)
+		return TINY_BUS_FAILED;
+		
+	list_remove(bus->slot_list, slot);
+	
+	result = pthread_mutex_unlock(bus->mutex);
+	if (result)
+		return TINY_BUS_FAILED;
+		
+	return TINY_BUS_SUCCEED;	
+}
+
+slot_t*
+slot_new(char *name)
+{
+	slot_t *slot;
+	int length, result;
+	
+	slot = (slot_t *)calloc(1, sizeof(slot_t));
+	if (slot == NULL)
+	{
+		tiny_bus_print_err(errno, "calloc");
+		return NULL;
+	}
+	
+	slot->mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	if (slot->mutex == NULL)
+	{
+		tiny_bus_print_err(errno, "malloc");
+		slot_free(slot);
+		return NULL;
+	}
+	
+	result = pthread_mutex_init(slot->mutex, NULL);
+	if (result)
+	{
+		tiny_bus_print_err(result, "pthread_mutex_init");
+		slot_free(slot);
+		return NULL;
+	}
+	
+	slot->subscription = (list_t *)calloc(1, sizeof(list_t));
+	if (slot->subscription == NULL)
+	{
+		tiny_bus_print_err(errno, "list_alloc");
+		slot_free(slot);
+		return NULL;		
+	}
+		
+	length = strlen(name);
+	length = (length >= sizeof(slot->slot_name)) ? (sizeof(slot->slot_name) - 1) : length;		
+	strncpy(slot->slot_name, name, length);
+}
+
+void
+slot_free(slot_t *slot)
+{
+	assert(slot);
+	
+	if (slot->mutex)
+		free(slot->mutex);
+		
+	if (slot->subscription)
+		list_free(slot->subscription);
+		
+	if (slot->msg_queue)
+		async_queue_destroy(slot->msg_queue);
+		
+	free(slot);
+}
--- tinybus.h
+++ tinybus.h
@@ -8,33 +8,128 @@
 
 #include <sys/types.h>
 #include <stdint.h>
-
-#include "queue.h"
+#include "atomic.h"
+#include "asyncqueue.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define TINY_BUS_MSG_EXIT		0xFFFFFFFF
+#define TINY_BUS_USER_MSG_BASE	0x00000000
+
 typedef struct _tiny_bus_msg_priv
 {
-	uint32_t	ref_count;
+	atomic_t	ref_count;
 	void *		data;
 	size_t		size;
 } tiny_msg_priv_t;
 
+typedef unsigned long message_id_t;
 typedef struct _tiny_bus_msg
 {
-	uint32_t	seq;
-	uint32_t	msg_id;
+	uint32_t		seq;
+	message_id_t	msg_id;
 	
 	tiny_msg_priv_t *msg_priv;
 	
-	// following is used when needing to transmit context for message processing
+	/*
+	 * following is used when needing to transmit context for message processing
+	 */
 	void *		user_data;
 	size_t		user_size;
 } tiny_msg_t;
 
+typedef enum _slot_status
+{
+	slot_not_ready		= 0,
+	slot_read_ready		= 0x0001,
+	slot_write_ready	= 0x0002,
+	slot_readwrite_read	= 0x0003
+} slot_status_t;
 
+/*
+ *  Each subscriber should own a slot, which is used to communicate with bus
+ *  a) A slot should own a message ID queue to identify its subscription
+ *  b) A slot should own a message queue to contain message
+ */
+typedef struct _slot
+{
+	/*
+	 * Messages can be posted to slot, it'multiple message IDs list(subscription);
+	 * when posting message, bus will check whether message id is in queue, true
+	 * then insert into slot's member: msg_queue.
+	 */
+	pthread_mutex_t	*mutex;
+	list_t			*subscription;	
+	
+	/*
+	 * for slot debug
+	 */
+	unsigned char	slot_name[32];
+	
+	/*
+	 * Identify whether slot is ready to recv message
+	 */ 
+	slot_status_t	slot_ready;
+	
+    /*
+	 * contain message posted by bus, will be processed by app module
+	 */
+	async_queue_t	*msg_queue;
+} slot_t;
+
+typedef struct _tiny_bus
+{
+	/*
+	 * bus thread
+	 */
+	pthread_t		*thread;
+	
+	/*
+	 * contain tiny_msg_t object
+	 */
+	async_queue_t	*msg_queue;
+	
+	/*
+	 * contain slot_t object
+	 */
+	pthread_mutex_t	*mutex;
+	list_t			*slot_list;
+} tiny_bus_t;
+
+typedef struct _publisher
+{
+	slot_t			*slot;
+	//...
+} publisher_t;
+
+typedef struct _subscriber
+{
+	slot_t			*slot;
+	
+	//...
+} subscriber_t;
+
+typedef struct _dustbin
+{
+	subscriber_t 	*subscriber;
+} dustbit_t;
+
+typedef enum
+{	
+	TINY_BUS_FAILED	= -1,
+	TINY_BUS_SUCCEED
+} tiny_bus_result_t;
+
+tiny_bus_t* tiny_bus_new(void);	// allocate a bus object
+void tiny_bus_destroy(tiny_bus_t *bus);	// destroy a bus object
+
+tiny_bus_result_t	tiny_bus_add_slot(tiny_bus_t *bus, slot_t *slot);
+tiny_bus_result_t	tiny_bus_del_slot(tiny_bus_t *bus, slot_t *slot);
+
+slot_t* slot_new(char *name);
+void slot_free(slot_t *slot);
 
 #ifdef __cplusplus
 }
